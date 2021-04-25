650b325984e7f35f07dc3a1ca7db1d1f7418822f
sany <sanyforpost@gmail.com>
2021-04-25 12:19:53 +0300

    AssemblyTitle was changed

--- FatfsToSpiffsConverter/Properties/AssemblyInfo.cs
+++ FatfsToSpiffsConverter/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("FatfsToSpiffsConverter")]
+[assembly: AssemblyTitle("W25Qxxx_Flasher")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("FatfsToSpiffsConverter")]
+[assembly: AssemblyProduct("W25Qxxx_Flasher")]
 [assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
